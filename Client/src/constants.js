4d1024ce3a7962753587d07f33f655789f3b1647
SALIL <salilmehan123@gmail.com>
2025-01-04 00:03:19 +0530

    updated deployed links

--- Client/src/constants.js
+++ Client/src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8080";
-export const CLIENT_URL = "http://localhost:5173";
+export const API_URL = "https://formbot-cuvette-jiwy.onrender.com";
+export const CLIENT_URL = "https://form-bot-cuvette.vercel.app/";
--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 // app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://form-bot-cuvette.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
